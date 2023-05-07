e009a3afb09a0108e6c26b5437a73cb5b165c0b7
anandafiqri <anandafiqri1403@gmail.com>
2023-05-07 13:36:33 +0700

    init

--- locationservice/locationservice/settings.py
+++ locationservice/locationservice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'locationservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'location-service',
+        'USER': 'postgres',
+        'PASSWORD' : 'qwertyui',
+        'HOST' : '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
